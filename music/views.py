b2a07b28de1a0e940d1ddb8effd58c043eee7526
ugochukwu <ugokingsley5@gmail.com>
2016-08-17 17:01:07 -0700

    the first 20 8/17/2016

---
+++ music/views.py
@@ -0,0 +1,20 @@
+from django.http import Http404
+from django.shortcuts import render
+from .models import Album
+
+
+def index(request):
+    all_albums=Album.objects.all()
+    context={'all_albums':all_albums}
+    return render(request,'music/index.html',context)
+
+def detail(request,album_id):
+   try:
+       album=Album.objects.get(pk=album_id)
+   except Album.DoesNotExist:
+       raise Http404("Albumdoes not exist")
+   return render(request,'music/detail.html',{'album':album})
+
+
+
+# Create your views here.
